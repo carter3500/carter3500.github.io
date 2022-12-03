8e286d1546535e03907e63fd090140789518e94c
carter3500 <114273315+carter3500@users.noreply.github.com>
2022-12-03 00:09:27 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
-<script>
-//country data in string array format
-var countries = ["Australia","France","India","Japan","United States"];
- 
-//This method calls on button click
-function dynamicDropdownList(){
- 
-    var dropdown = document.createElement("select");
-    for(var i=0;i<countries.length;i++){     
-        var opt = document.createElement("option"); 
-        opt.text = countries[i];
-        opt.value = countries[i];
-        dropdown.options.add(opt);      
-    }
-     
-    //Load the dynamically created dropdown in container
-    var container=document.getElementById("dropdown-container");
-    container.appendChild(dropdown);
+let cartTotal = 0
+
+function addToCart(){
+  cartTotal += 1;
+  sessionStorage.setItem("cartTot", cartTotal);
+  updateCart();
 }
- 
-</script>
+
+function updateCart(){
+  cartTotal = parseInt(sessionStorage.getItem("cartTot"));
+  document.getElementById("cartnumber").innerHTML = cartTotal;
+}
+
+updateCart();
